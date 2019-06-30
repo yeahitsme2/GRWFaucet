ad91b020f088c7836b407ddd7f81287cba839e7f
iamunick <why@iamunick.com>
2019-06-30 14:48:40 -0400

    Use .env file to set faucet config options

--- config/faucet.php
+++ config/faucet.php
@@ -1,12 +1,13 @@
 <?php
 return [
-    'ticker' => 'GRW',
-    'coinName' => 'Growthcoin',
-    'minReward' => 1000000, // in smallest denomination (satoshis)
-    'maxReward' => 11000000, // in smallest denomination (satoshis)
-    'coinDecimals' => 6,
-    'payoutCooldownSeconds' => 60,
+    'ticker' => env( 'COIN_TICKER',  'GRW' ),
+    'coinName' => env( 'COIN_NAME', 'Growthcoin' ),
+    'faucetAddress' => env( 'FAUCET_ADDRESS', ''),
+    'minReward' => env( 'MIN_REWARD', 1000000 ), // in smallest denomination (satoshis)
+    'maxReward' => env( 'MAX_REWARD', 11000000), // in smallest denomination (satoshis)
+    'coinDecimals' => env( 'COIN_DECIMALS', 6 ),
+    'payoutCooldownSeconds' => env( 'PAYOUT_COOLDOWN_TIMER', 4 * 60 * 60 ), // in seconds, here 4 hours
     'recaptchaSiteKey' => env( 'RECAPTCHA_SITE_KEY', '' ),
     'recaptchaPrivateKey' => env( 'RECAPTCHA_PRIVATE_KEY', '' ),
-
+    'recaptchaEnable' => env( 'RECAPTCHA_ENABLE', 0),
 ];
